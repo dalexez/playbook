9e65ac0db9388465100d50da6760dbbf85a1ae01
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-13 13:01:02 -0500

    Add Example 0-4

---
+++ weekly_mission_2/examples0/examples0-4.js
@@ -0,0 +1,23 @@
+// Ejemplo 4: Objeto con métodos
+const pet = {
+    name: "Tulio",
+    // Esta es una función que se guarda como propiedad
+    sayHello: function(){
+      // this.name hace referencia a la propiedad del objeto
+      console.log(`${this.name} te saluda en español!`)
+    }
+  }
+  
+  console.log("Ejemplo 4: Objeto con métodos")
+  pet.sayHello()
+  
+  // Ejemplo 5: Objeto con método que recibe parámetros
+  const myPet = {
+    name: "Woopa",
+    sayHelloToMyPet: function(yourPet){
+      console.log(`${this.name} say's hello to ${yourPet}`)
+    }
+  }
+  
+  console.log("Ejemplo 5: Objeto con método que recibe parámetros")
+  myPet.sayHelloToMyPet("Tulio")
