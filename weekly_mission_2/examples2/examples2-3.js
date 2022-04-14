d1202b47278a88bce1d399a4f67fda7d94e13b61
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-14 12:43:38 -0500

    add examples2- static methods

--- weekly_mission_2/examples2/examples2-3.js
+++ weekly_mission_2/examples2/examples2-3.js
@@ -8,7 +8,7 @@ class Student {
 	}
 }
 
-// Crear un objeto de la clase Student (esto se le llama instanciación)
+// Crear un objeto de la clase Student (esto se le llama instanciación) ******NOTA******* IMPORTANTISIMO
 const carloStudent = new Student("Carlo", 12, ["NodeJs", "Python"])
 console.log("Ejemplo 3: Instanciar un objeto con atributos")
 console.log(carloStudent)
