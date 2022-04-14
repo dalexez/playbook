fd0ac008e10755fbffa6b011e7c4ea427493d59d
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-14 12:10:21 -0500

    add example2-3 constructor making a new objet

--- weekly_mission_2/examples2/examples2-3.js
+++ weekly_mission_2/examples2/examples2-3.js
@@ -7,3 +7,8 @@ class Student {
     this.subjects = subjects
 	}
 }
+
+// Crear un objeto de la clase Student (esto se le llama instanciación)
+const carloStudent = new Student("Carlo", 12, ["NodeJs", "Python"])
+console.log("Ejemplo 3: Instanciar un objeto con atributos")
+console.log(carloStudent)
