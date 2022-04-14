9542bc950897d847e4dc4a9a31bbab179390bbc2
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-13 19:35:49 -0500

    changes to example 1-16 and example 2-1 added

--- weekly_mission_2/examples1/examples1-16.js
+++ weekly_mission_2/examples1/examples1-16.js
@@ -6,16 +6,29 @@ const users = [
     { name: 'D', age: 22 },
   ]
   
-  users.sort((a, b) => { // podemos invocar una función
-    if (a.age < b.age) return -1
-    console.log("El elemento " + a.name + " es menor que: " + b.name)
-    if (a.age > b.age) return 1
-    console.log("El elemento " + a.name + " es mayor que: " + b.name)
-    return 0
+  users.sort((a, b) => {
+    console.log(a,b)
+    console.log(users)
+    return a.age - b.age
+    //Si return es < 0   a es menor que b
+    //Si return es > 0   a es mayor que b
+    //return = 0  a y b son iguales
+
   })
-  
   console.log("Ejemplo 16: Ordenando una lista de objetos por la edad")
   console.log(users) // sorted ascending
 
 
 //  console.log(`${this.name} te saluda en español!`)
+
+
+
+
+
+
+// // podemos invocar una función
+// if (a.age < b.age) return -1
+// //console.log("El elemento " + a.name + " es menor que: " + b.name)
+// if (a.age > b.age) return 1
+// //console.log("El elemento " + a.name + " es mayor que: " + b.name)
+// return 0
