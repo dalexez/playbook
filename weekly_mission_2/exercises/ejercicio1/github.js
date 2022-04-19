4e694f319857e88683b81ed56f988baff1d92f5a
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-19 13:57:13 -0500

    agregando issue

--- weekly_mission_2/exercises/ejercicio1/github.js
+++ weekly_mission_2/exercises/ejercicio1/github.js
@@ -15,8 +15,30 @@ const repo = {
     }
    }
    
-   console.log("Nombre del repo:" + repo.name)
-   console.log("Issues totales: " + repo.getTotalIssues())
-   console.log(repo.getGeneralInfo())
+console.log("Nombre del repo:" + repo.name)
+console.log("Issues totales: " + repo.getTotalIssues())
+console.log(repo.getGeneralInfo())
+
+const issue = {
+  title: "Semana 2",
+  repositoryNameAssociated: "LaunchXBackend",
+  status: "Active",
+  numberOfComments: 23,
+  labels: "Live2",
+  author: "MC",
+  dateCreated: "17/04/2022",
+  lastUpdate: "2 days ago",
+  getTitleAndAuthor: function(){
+    return this.title + this.author
+  },
+  getGeneralInfo: function(){
+    return `This issue ${this.title} was created by ${this.author}`
+  }
+}
+
+console.log("Nombre del issue: " + issue.title)
+console.log("Comentarios totales en el issue: " + issue.numberOfComments)
+console.log(issue.getGeneralInfo())
+
 
    
